c2349b15cb4e0165bda4ab8a766ce399113e7b7a
Akshay Sawant <akshaysawant917@gmail.com>
2025-07-28 22:12:45 +0530

    updated routes import

--- app/login/actions.js
+++ app/login/actions.js
@@ -1,7 +1,7 @@
 'use server';
 
 import dbConnect from '@/lib/mongoose';
-import User from '@/models/User';
+import User from '../..models/User';
 import bcrypt from 'bcryptjs';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
